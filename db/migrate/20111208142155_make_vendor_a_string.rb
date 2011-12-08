0d9b731bf14ffc7d799bea92649ddbaf4cc66ea6
Felix Rabe <public@felixrabe.net>
2011-12-08 15:29:48 +0100

    Implement vendor string migration

--- db/migrate/20111208142155_make_vendor_a_string.rb
+++ db/migrate/20111208142155_make_vendor_a_string.rb
@@ -1,7 +1,16 @@
 class MakeVendorAString < ActiveRecord::Migration
+  class Software < ActiveRecord::Base
+  end
+
   def up
+    add_column :softwares, :vendor_str, :string
+    Software.reset_column_information
+    Software.all.each { |s| s.update_attributes! :vendor_str => s.vendor.name }
+    drop_table :vendors
+    rename_column :softwares, :vendor_str, :vendor
   end
 
   def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
